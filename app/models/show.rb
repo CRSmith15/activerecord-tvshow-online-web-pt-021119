2056183d19438cffe4da32e69383cb73f00c4a0d
Chance Smith <chancesmith97@hotmail.com>
2019-08-10 02:45:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,12 @@ class Show < ActiveRecord::Base
   def Show::most_popular_show
     Show.order(rating: :desc).first 
   end
+  
+  def Show::lowest_rating
+    Show.minimum(:rating)
+  end
+  
+  def Show::least_popular
+    Show.order(rating: :asc).first
+  end
 end
